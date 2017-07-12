f5b73c82f6b74b414600ed492b0c79f0d6696247
5CLASS-184 <5CLASS-184@HB01-11>
2017-07-12 17:23:27 +0900

    HAEIN

--- src/main/java/com/project/cgv/contoller/BoardController.java
+++ src/main/java/com/project/cgv/contoller/BoardController.java
@@ -1,9 +1,16 @@
 package com.project.cgv.contoller;
 
+import java.util.HashMap;
+import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.mysql.fabric.xmlrpc.base.Params;
 import com.project.cgv.service.BoardService;
 
 @Controller
@@ -11,10 +18,97 @@ import com.project.cgv.service.BoardService;
 public class BoardController {
 	
 	@Autowired private BoardService bService;
+	/*@Autowired private BoardService bService;*/
 	
 	@RequestMapping("/boardlist")
-	public String boardList() {
+	public String boardList(Model model,@RequestParam(required = false) String keyword, @RequestParam(defaultValue = "0") int type,@RequestParam(defaultValue="1")int page) {
+		
+		HashMap<String,Object> params = new HashMap<String,Object>();		
+		
+		params.put("keyword", keyword);
+		params.put("type", type);
+		
+		if(type==1){
+			//제목
+			params.put("b_title", keyword);
+		}else if(type==2){
+			//내용
+			params.put("b_content", keyword);
+		}else if(type==3){
+			//제목+내용
+			params.put("b_title", keyword);
+			params.put("b_content", keyword);
+		}else if(type==4){
+			//작성자
+			params.put("b_writer", keyword);
+		}
+		
+		HashMap<String,Object> viewData = bService.getBoardList(params, page); 		
+
+		model.addAttribute("viewData", viewData);
 		return ".board.boardList";
+		
 	}
+	
+	@RequestMapping("/boardwrite")
+	public String WriteForm(){
+		
+		return ".board.boardWrite";
+	}
+	
+	@RequestMapping("/write")
+	public String boardWrite(@RequestParam HashMap<String, Object> params, MultipartFile attachfile){
+
+		bService.writeBoard(params,attachfile);
+		return "redirect:boardlist";
+		
+	}
+	
+	@RequestMapping("/boardview")
+	public String ViewForm(Model model, @RequestParam int num, HttpSession session){
+
+		HashMap<String, Object> member = (HashMap<String, Object>)session.getAttribute("member");
+		HashMap<String, Object> viewBoard = bService.viewBoard(num);
+				
+			if(member != null && viewBoard.get("id") != null && (viewBoard.get("id")).equals(member.get("id"))){
+				model.addAttribute("mine", true);
+				
+			}else {
+				model.addAttribute("mine", false);
+			}
+		
+		model.addAttribute("viewBoard", viewBoard);
+		return ".board.boardView";
+		
+	}	
+	
+	@RequestMapping("/delete")
+	public String boardDelete(@RequestParam int num){
+		
+		bService.deleteBoard(num);
+		return "redirect:boardlist";
+		
+	}
+	
+	@RequestMapping("/boardupdate")
+	public String UpdateForm(Model model, @RequestParam int num){
+		
+		HashMap<String, Object> viewBoard = bService.viewBoard(num);
+		model.addAttribute("viewBoard", viewBoard);
+		return ".board.boardUpdate";
+		
+	}
+	
+	@RequestMapping("/update")
+	public String boardUpdate(@RequestParam HashMap<String, Object> params, MultipartFile attachfile){
+		
+		bService.updateBoard(params,attachfile);
+		return "redirect:boardlist";
+		
+	}
+		
+	
+	
+	
 	
 }
